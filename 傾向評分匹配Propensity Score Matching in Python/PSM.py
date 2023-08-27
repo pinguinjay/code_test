cc63a5d6c4e5548ca9cb1b4e68d63dbf13fdb8eb
CHOU, TZU-CHIEH <83256594+pinguinjay@users.noreply.github.com>
2023-08-27 19:05:57 +0800

    Update PSM.py

--- 傾向評分匹配Propensity Score Matching in Python/PSM.py
+++ 傾向評分匹配Propensity Score Matching in Python/PSM.py
@@ -38,3 +38,50 @@ if p > alpha:
     print('same distributions/same group mean (fail to reject H0 - we do not have enough evidence to reject H0)')
 else:
     print('different distributions/different group mean (reject H0)')
+
+# student's t-test for facebook likes (dependent variable)
+from scipy.stats import ttest_ind
+
+print(df_control.fb_likes.mean(), df_treatment.fb_likes.mean())
+
+# compare samples
+_, p = ttest_ind(df_control.fb_likes, df_treatment.fb_likes)
+print(f'p={p:.3f}')
+
+# interpret
+alpha = 0.05  # significance level
+if p > alpha:
+    print('same distributions/same group mean (fail to reject H0 - we do not have enough evidence to reject H0)')
+else:
+    print('different distributions/different group mean (reject H0)')
+
+# choose features for propensity score calculation
+X = df[['prom_length', 'price', 'discount_pct', 'coupon_duration', 'featured', 'limited_supply']]
+y = df['treatment']
+
+X.head()
+
+# use logistic regression to calculate the propensity scores
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression()
+lr.fit(X, y)
+
+# get the coefficients 
+lr.coef_.ravel()  # or reshape(-1) refers to an unknown dimension, often used to flatten the array
+
+# get the feature names
+X.columns.to_numpy()
+
+# combine features and coefficients into a dataframe
+coeffs = pd.DataFrame({
+    'column':X.columns.to_numpy(),
+    'coeff':lr.coef_.ravel(),
+})
+coeffs
+
+# prediction
+pred_binary = lr.predict(X)  # binary 0 control, 1, treatment
+pred_prob = lr.predict_proba(X)  # probabilities for classes
+
+print('the binary prediction is:', pred_binary[0])
+print('the corresponding probabilities are:', pred_prob[0])
