bf16920cba36c974c357cf832a7a5c60411cb917
CHOU, TZU-CHIEH <83256594+pinguinjay@users.noreply.github.com>
2023-08-27 19:04:35 +0800

    Update PSM.py

--- 傾向評分匹配Propensity Score Matching in Python/PSM.py
+++ 傾向評分匹配Propensity Score Matching in Python/PSM.py
@@ -2,3 +2,39 @@
 the code is from https://www.kaggle.com/code/harrywang/propensity-score-matching-in-python
 """
 
+# load packages
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import math
+sns.set()  # set the style
+
+# read data
+df = pd.read_csv('../input/propensity-score-matching/groupon.csv')
+df.info()
+
+df.head()
+
+# check the means for control and treatment
+df.groupby('treatment').mean()
+
+# separate control and treatment for t-test
+df_control = df[df.treatment==0]
+df_treatment = df[df.treatment==1]
+
+# student's t-test for revenue (dependent variable)
+from scipy.stats import ttest_ind
+
+print(df_control.revenue.mean(), df_treatment.revenue.mean())
+
+# compare samples
+_, p = ttest_ind(df_control.revenue, df_treatment.revenue)
+print(f'p={p:.3f}')
+
+# interpret
+alpha = 0.05  # significance level
+if p > alpha:
+    print('same distributions/same group mean (fail to reject H0 - we do not have enough evidence to reject H0)')
+else:
+    print('different distributions/different group mean (reject H0)')
