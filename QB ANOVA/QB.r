aea950ee45940b4523ff93ec5c0827d1e1aad284
CHOU, TZU-CHIEH <83256594+pinguinjay@users.noreply.github.com>
2023-09-10 14:04:28 +0800

    Update QB.r

--- QB ANOVA/QB.r
+++ QB ANOVA/QB.r
@@ -29,3 +29,13 @@ for (colnumber in c(2:length(NL1))) { #length(NL1)，幾個變數
 formula <- as.formula(paste0("cbind(", paste(names(NL1)[-1], collapse = ","), ") ~ Group"))
 fit<- aov(formula,data = NL1)
 summary(fit)
+#把ANOVA寫成word檔案(.doc)
+#注意，目前只知道可以一個一個更改變數用
+#雖然麻煩，但比一個字一個字打還方便了
+library(apaTables)
+V19_ANOVA<- lm(V19~Group, data = NL1)
+apa.aov.table(V19_ANOVA, filename = "D:\\V19_ANOVA.doc")
+#TD.r%  Y/C  Int%  Y.r/rush  Y.r/G  rush/G  Sk%  Sk/G  SckYds/G  GWD/G	
+#V1  V2  V3  V4  V5  V6  V7  V8  V9  V10
+#Cmp%  PR  TD.p%  Y/A  NY/A  Pro_bowler  GS/year  1D/rush  Fmb/G
+#V11  V12  V13  V14  V15  V16  V17  V18  V19
